623de00bf4e09d6ff6c69170bdf89590e4d02c6a
MaysonD4rk <maysondamarante@hotmail.com>
2022-11-17 20:12:22 -0300

    iniciando processo de autenticação de user 3

--- public/js/account_settings.js
+++ public/js/account_settings.js
@@ -19,7 +19,7 @@ async function changePass(email, token) {
                         password: newPass
                     }, {
                         headers: {
-                            'Authorization': `Basic ${token}`
+                            'Authorization': `Bearer ${token}`
                         }
                     })
 
