ec28e62e9145e5e8bde9c9e379eca198ed206252
MaysonD4rk <maysondamarante@hotmail.com>
2022-12-06 01:28:30 -0300

    atualização

--- public/js/nextPreview.js
+++ public/js/nextPreview.js
@@ -1,7 +1,7 @@
     var url = window.location.href;
     //valor precisa ser diferente na produção. deixar console.log para n ter problemas depois
     console.log(url)
-    var offset = url.slice(22);
+    var offset = url.slice(27);
     console.log(offset)
     var sep = offset.split('?');
 
